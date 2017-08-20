6796d94171ab63899a1636f4c61eee5928a703f3
nishadshamsudeen <nishads52@gmail.com>
2017-08-20 14:42:38 +0530

    FinalCommit

--- ButtonSample/app/src/main/java/com/example/user/buttonsample/MainActivity.java
+++ ButtonSample/app/src/main/java/com/example/user/buttonsample/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.user.buttonsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mToastButton = null;
+    private EditText mUserNameEditText = null;
+    private EditText mPasswordEditText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mToastButton = (Button) findViewById(R.id.login);
+        mUserNameEditText = (EditText) findViewById(R.id.name);
+        mPasswordEditText = (EditText) findViewById(R.id.pass);
+
+        mToastButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "Welcome " + mUserNameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
